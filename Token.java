fc79f4fcdc3f1a700b3d67a2a5e9e96848158475
Lorenzo Fundaró García <lorenzofundaro@yahoo.com>
2011-12-03 12:43:18 -0430

    Documentclustering.java: fetch info from twitter, 	ft-idf for every tweet and clustering. 	README: new. 	Token.java: container for (Tweet,vsm). 	preguntas: new.

---
+++ Token.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package documentclustering;
+
+import twitter4j.Tweet;
+
+/**
+ *
+ * @author Lorenzo Fundaró <lorenzofundaro [at] yahoo.com>
+ */
+public class Token {
+    Tweet tweet;
+    double[] vsm;
+    String fromUser;
+    double tf;
+    
+    public Token(Tweet t, double[] v) {
+        tweet = t;
+        vsm = v;
+        fromUser = t.getFromUser();
+        tf = 0.0;
+    }
+    
+    public Tweet getTweet() {
+        return tweet;
+    }
+    
+    public double[] getVsm() {
+        return vsm;
+    }
+
+    public String getFromUser() {
+        return fromUser;
+    }
+    
+    public void computeTermWeight(int[] dfi, int nD) {
+        for(int i = 0; i < vsm.length; i++) {
+            if (dfi[i] > 0.0) {
+                vsm[i] = vsm[i]*(Math.log((double)nD/(double)dfi[i]));
+            }
+        }
+    }
+}
