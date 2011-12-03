89f83e3de9a4a8b244173fafd59aa2a2c9ba505c
Lorenzo Fundaró García <lorenzofundaro@yahoo.com>
2011-12-02 22:57:15 -0430

    DocumentClustering.java: testing Twitter API.

--- DocumentClustering.java
+++ DocumentClustering.java
@@ -4,16 +4,41 @@
  */
 package document.clustering;
 
+import java.util.List;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Tweet;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
 /**
  *
  * @author Lorenzo Fundaró <lorenzofundaro [at] yahoo.com>
  */
 public class DocumentClustering {
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        if (args.length < 1) {
+            System.out.println("java twitter4j.examples.search.SearchTweets [query]");
+            System.exit(-1);
+        }
+        Twitter twitter = new TwitterFactory().getInstance();
+        try {
+            QueryResult result = twitter.search(new Query(args[0]));
+            List<Tweet> tweets = result.getTweets();
+            for (Tweet tweet : tweets) {
+                System.out.println("@" + tweet.getFromUser() + " - " + tweet.getText());
+            }
+            System.exit(0);
+        } catch (TwitterException te) {
+            te.printStackTrace();
+            System.out.println("Failed to search tweets: " + te.getMessage());
+            System.exit(-1);
+        }
     }
 }
