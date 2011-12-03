fc30e1f6f95a417a4d2f462903c3993215afc34b
Jose <josej30@gmail.com>
2011-12-03 16:55:22 -0430

    anadiendo las stopwords en espanol. Limpiando los inputs de cualquier caracter extrano como signos de puntuacion, exclamacion, etc. Anadi como etiqueta de clase para cada elemento del cluster el texto del tweet que lo va a identificar. Tambien imprimo al final el numero de elementos que quedaron en cada cluster y los tweets que quedaron agrupados en cada uno. No hay resultados buenos con primarias y celac

--- ClusteringTest/src/clusteringtest/ClusteringTest.java
+++ ClusteringTest/src/clusteringtest/ClusteringTest.java
@@ -27,24 +27,39 @@ public class ClusteringTest {
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
         /* Load a dataset */
-        Dataset data = FileHandler.loadDataset(new File("test.txt"),11, ",");
+        Dataset data = FileHandler.loadDataset(new File("test2.txt"),4, ",");
         /*
          * Create a new instance of the KMeans algorithm, with no options
          * specified. By default this will generate 4 clusters.
          */
-        Clusterer km = new KMeans(2,100);
+        Clusterer km = new KMeans(3,100);
         /*
          * Cluster the data, it will be returned as an array of data sets, with
          * each dataset representing a cluster
          */
         Dataset[] clusters = km.cluster(data);
+
         System.out.println("Cluster count: " + clusters.length);
-        TreeSet<java.lang.Object> p = (TreeSet<java.lang.Object>) clusters[1].classes();
-        //        p.toString();
-        System.out.println(p);
-//        System.out.println(clusters[3].size());
-        Set<Instance> s = clusters[1].kNearest(1, clusters[1].get(1), new EuclideanDistance());
-        System.out.println(s);
+
+        System.out.println(" === Cluster 1 size: " + clusters[0].size());
+        for (int i=0;i<clusters[0].size();i++){
+            System.out.println(clusters[0].get(i));
+        }
+        System.out.println(" === Cluster 2 size: " + clusters[1].size());
+        for (int i=0;i<clusters[1].size();i++){
+            System.out.println(clusters[1].get(i));
+        }
+        System.out.println(" === Cluster 3 size: " + clusters[2].size());
+        for (int i=0;i<clusters[2].size();i++){
+            System.out.println(clusters[2].get(i));
+        }
+
+//        TreeSet<java.lang.Object> p = (TreeSet<java.lang.Object>) clusters[1].classes();
+//        //        p.toString();
+//        System.out.println(p);
+////        System.out.println(clusters[3].size());
+//        Set<Instance> s = clusters[1].kNearest(1, clusters[1].get(1), new EuclideanDistance());
+//        System.out.println(s);
 //        ClusterEvaluation sse= new SumOfCentroidSimilarities();
 //        /* Measure the quality of the clustering */
 //        double score=sse.score(clusters);
--- Documentclustering.java
+++ Documentclustering.java
@@ -10,6 +10,8 @@ import java.util.List;
 import java.util.TreeSet;
 import net.sf.javaml.clustering.Clusterer;
 import net.sf.javaml.clustering.KMeans;
+import net.sf.javaml.clustering.evaluation.ClusterEvaluation;
+import net.sf.javaml.clustering.evaluation.SumOfSquaredErrors;
 import net.sf.javaml.core.Dataset;
 import net.sf.javaml.core.DefaultDataset;
 import net.sf.javaml.core.DenseInstance;
@@ -26,29 +28,84 @@ import twitter4j.TwitterFactory;
  * @author Lorenzo Fundaró <lorenzofundaro [at] yahoo.com>
  */
 public class Documentclustering {
-    
+
+    public static String[] stopwords = {    "a",
+                                            "el",
+                                            "la",
+                                            "lo",
+                                            "las",
+                                            "les",
+                                            "los",
+                                            "con",
+                                            "de",
+                                            "por",
+                                            "que",
+                                            "me",
+                                            "para",
+                                            "se",
+                                            "un",
+                                            "una",
+                                            "uno",
+                                            "unos",
+                                            "unas"
+                                            };
+
+    public static boolean isStopword(String term, String[] stopwords){
+        for (int i=0;i<stopwords.length;i++)
+            // Removing stopwords and links
+            if (stopwords[i].compareToIgnoreCase(term)==0 || term.startsWith("http"))
+                return true;
+        return false;
+    }
+
+    public static String clean(String dirty){
+
+        return dirty.replaceAll("[.,;:\"!]", "");
+
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
         Twitter twitter = new TwitterFactory().getInstance();
+
         try {
+
             String[] tokens;
             TreeSet<String> s = new TreeSet<String>();
+
             QueryResult resultPrimarias = twitter.search(new Query(args[0]));
             List<Tweet> tweetsPrimarias = resultPrimarias.getTweets();
             for ( Tweet tweet : resultPrimarias.getTweets()) {
                 tokens = tweet.getText().toLowerCase().split("\\s");
-                s.addAll(Arrays.asList(tokens));
+
+                // Cleaning the words and looking for stopwords
+                for (int i = 0; i<tokens.length; i++){
+                    if ( !isStopword(tokens[i],stopwords) && tokens[i].length()!=0)
+                        s.add(clean(tokens[i]));
+                }
+
+
             }
             //                System.out.println(tweet.getText());
+
             QueryResult resultCelac = twitter.search(new Query(args[1]));
             List<Tweet>  tweetsCELAC = resultCelac.getTweets();
             for ( Tweet tweet : resultCelac.getTweets() ) {
                 tokens = tweet.getText().toLowerCase().split("\\s");
-                s.addAll(Arrays.asList(tokens));
+
+                // Cleaning the words and looking for stopwords
+                for (int i = 0; i<tokens.length; i++){
+                    if ( !isStopword(tokens[i],stopwords) && tokens[i].length()!=0)
+                        s.add(clean(tokens[i]));
+                }
+                
             }
+
             System.out.println(s.size());
+
             //            System.out.println(s);
             // Preparar colección
             Token[] col = new Token[tweetsPrimarias.size()
@@ -74,11 +131,14 @@ public class Documentclustering {
             int[] docFreq  = new int[s.size()];
             int setIndex = 0;
             while (it.hasNext()) {
+
                 String term = it.next();
+
                 if (term.length() == 0) {
                     setIndex++;
                     continue;
                 }
+
                 boolean present = false;
                 for(int i = 0; i < col.length; i++) {
                     String tweetText = col[i].tweet.getText();
@@ -101,22 +161,29 @@ public class Documentclustering {
             Instance[] instances = new Instance[tweetsCELAC.size() +
                     tweetsPrimarias.size()];
             for(int i = 0; i < instances.length; i++) {
-                instances[i] = new DenseInstance(col[i].getVsm());
+                instances[i] = new DenseInstance(col[i].getVsm(),col[i].getTweet().getText());
             }
             Dataset data = new DefaultDataset();
             data.addAll(Arrays.asList(instances));
-            Clusterer km = new KMeans(2,1000);
+            Clusterer km = new KMeans(2,100);
             Dataset[] clusters = km.cluster(data);
             System.out.println("Cluster count: " + clusters.length);
-            TreeSet<java.lang.Object> p = (TreeSet<java.lang.Object>) clusters[0].classes();
-            TreeSet<java.lang.Object> p2 = (TreeSet<java.lang.Object>) clusters[1].classes();
+            //TreeSet<java.lang.Object> p = (TreeSet<java.lang.Object>) clusters[0].classes();
+            //TreeSet<java.lang.Object> p2 = (TreeSet<java.lang.Object>) clusters[1].classes();
             //        p.toString();
-            System.out.println(p);
-            System.out.println("=================================");
-            System.out.println(p2);
-            //             //        System.out.println(clusters[3].size());
-            //             Set<Instance> set = clusters[1].kNearest(1, clusters[1].get(1), new EuclideanDistance());
-            //             System.out.println(set);
+            
+            System.out.println(" ========== Cluster #1 size: "+clusters[0].size());
+            //for (int i=0;i<clusters[0].size();i++) System.out.println(clusters[0].get(i).classValue());
+            
+            System.out.println(" ========== Cluster #2 size: "+clusters[1].size());
+            //for (int i=0;i<clusters[1].size();i++) System.out.println(clusters[1].get(i).classValue());
+
+            ClusterEvaluation sse= new SumOfSquaredErrors();
+            /* Measure the quality of the clustering */
+            double score=sse.score(clusters);
+            System.out.println("Score: "+score);
+
+            System.out.println(s.toString());
             
         } catch (TwitterException te) {
             System.out.println("Failed to search tweets: " + te.getMessage());
